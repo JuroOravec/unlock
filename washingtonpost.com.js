f70fab7cdb8ba65d201ca323153d303dba7a44b9
Juro Oravec <juraj.oravec.josefson@gmail.com>
2020-03-17 10:48:03 +0100

    feat: add washingtonpost.com script

---
+++ washingtonpost.com.js
@@ -0,0 +1,16 @@
+[
+  {
+    version: "1.0.0",
+    versionDate: "2020-03-17",
+    script() {
+      const body = window.document.body;
+      window.document.querySelector('html').style = "";
+      body.style = "";
+      Array.from(body.childNodes).slice(-6).forEach(el => el.remove());
+      const res = await fetch(location.href);
+      const b = await res.clone().text();
+      const htmlDoc = new DOMParser().parseFromString(b, 'text/html');
+      body.querySelector('main').replaceWith(htmlDoc.querySelector('main'));
+    } 
+  }
+]
