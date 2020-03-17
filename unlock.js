97c27ea2b3eff226180557f5e7e89416c1686d12
Juro Oravec <juraj.oravec.josefson@gmail.com>
2020-03-17 12:26:32 +0100

    feat: unlock script

---
+++ unlock.js
@@ -0,0 +1,11 @@
+((ctx) => {
+  const mainURL = "https://raw.githubusercontent.com/JuroOravec/unlock/master/main.js";
+
+  const res = await fetch(mainURL);
+  if (!res.ok) {
+    console.warn(`[Unlock] Could not load unlock/main.js`);
+    return;
+  }
+  return eval(await res.clone().text()).unlock();
+
+})(window)
